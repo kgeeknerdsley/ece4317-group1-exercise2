49bd79d8e20abd2e1b314fd3aa6029643978f114
Kevin Worsley <kgeekmail@gmail.com>
2020-09-30 23:25:14 -0700

    Filled out getNeighbor function

--- bfs.py
+++ bfs.py
@@ -1,11 +1,21 @@
 from puzzle import Puzzle
 
-#perform depth first search!
+#perform breadth first search!
 
 #just some tests to show how the class works
-testPuzzle = Puzzle([[1,2,3],[4,5,0],[6,7,8]])
-testPuzzle.printBoard()
-print(testPuzzle.getTile(0,2))
 
-stringVer = testPuzzle.printBoardString()
-print(stringVer)
+#testPuzzle.printBoard()
+#print(testPuzzle.getTile(0,2))
+
+#stringVer = testPuzzle.printBoardString()
+#print(stringVer)
+
+testPuzzle = Puzzle([[1,2,3],[4,5,0],[6,7,8]])
+print("Unmodified board")
+testPuzzle.printBoard()
+
+print("Attempting to find neighbors")
+possibilities = testPuzzle.getNeighbors()
+print(possibilities)
+
+#testPuzzle.printBoard()
--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,5 @@
+import copy
+
 class Puzzle:
     def __init__(self, boardData):
         self.board = boardData #board is a 3x3 array (technically a list, but w/e)
@@ -7,11 +9,60 @@ class Puzzle:
     def isGoal(self):
         return False
 
-    #TODO: FILL OUT
+    #TODO: Make a function for the move process
     #Takes in itself, returns list of possible moves to make
     #TODO: should each new neighbor be a new object, or just return a list of the boards to check out?
     def getNeighbors(self):
-        pass
+        neighborBoards = [] #holds all the new boards we make
+        holeRow = 0
+        holeCol = 0
+
+        #find the hole in our board: DONE
+        #check four possible moves: up, down, left, right
+        #only calculate the move if we know it's legal
+
+        #iterate over the array. when we find the hole, mark its location!
+        for holeSearchR in range(len(self.board)):
+            for holeSearchC in range(len(self.board[holeSearchR])):
+                if(self.getTile(holeSearchR,holeSearchC) == 0):
+                    holeRow = holeSearchR
+                    holeCol = holeSearchC
+
+        print("Hole located at (" + str(holeRow) + " " + str(holeCol) + ")\n")
+
+        #attempt up move
+        if(holeRow-1 >= 0):
+            tempVal = self.getTile(holeRow-1, holeCol)
+            copiedBoard = copy.deepcopy(self.board)
+            copiedBoard[holeRow-1][holeCol] = 0
+            copiedBoard[holeRow][holeCol] = tempVal
+            neighborBoards.append(copiedBoard)
+        
+        #attempt left move
+        if(holeCol-1 >= 0):
+            tempVal = self.getTile(holeRow, holeCol-1)
+            copiedBoard = copy.deepcopy(self.board)
+            copiedBoard[holeRow][holeCol-1] = 0
+            copiedBoard[holeRow][holeCol] = tempVal
+            neighborBoards.append(copiedBoard)
+
+        #attempt right move
+        if(holeCol+1 <= 2):
+            tempVal = self.getTile(holeRow, holeCol+1)
+            copiedBoard = copy.deepcopy(self.board)
+            copiedBoard[holeRow][holeCol+1] = 0
+            copiedBoard[holeRow][holeCol] = tempVal
+            neighborBoards.append(copiedBoard)
+
+        #attempt right move
+        if(holeRow+1 <= 2):
+            tempVal = self.getTile(holeRow+1, holeCol)
+            copiedBoard = copy.deepcopy(self.board)
+            copiedBoard[holeRow+1][holeCol] = 0
+            copiedBoard[holeRow][holeCol] = tempVal
+            neighborBoards.append(copiedBoard)
+
+        return neighborBoards
 
     #TODO: FILL OUT
     #Returns the Hamming code for itself
@@ -21,6 +72,12 @@ class Puzzle:
     #Takes in row and column, returns the value at that spot
     def getTile(self,row,col):
         return self.board[row][col]
+
+    def changeTile(self, newValue, row, col):
+        self.board[row][col] = newValue
+
+    def copyBoard(self):
+        return self.board
 
     #TODO: FILL OUT
     #Takes in a different board, returns true if the same, false otherwise
